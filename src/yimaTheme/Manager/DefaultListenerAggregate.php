6395c0ecd79ac7651f50cd1a542790cdd8aba090
Payam Naderi <naderi.payam@gmail.com>
2014-08-22 19:33:59 +0430

    drop widgetize to separated module

--- src/yimaTheme/Manager/DefaultListenerAggregate.php
+++ src/yimaTheme/Manager/DefaultListenerAggregate.php
@@ -61,8 +61,6 @@ class DefaultListenerAggregate extends Manager implements
 
         $events->attach('Zend\Mvc\Controller\AbstractController', MvcEvent::EVENT_DISPATCH,array($this, 'onDispatchSpecLayout'), -99);
         $events->attach('Zend\Mvc\Application', MvcEvent::EVENT_DISPATCH_ERROR, array($this,'onDispatchSpecLayout'), -99);
-
-        $events->attach('Zend\Mvc\Application', MvcEvent::EVENT_RENDER, array($this, 'onRenderWidgetator'), -9000);
     }
 
     // --- Events Methods ---------------------------------------------------------------------------------------------------------------------
@@ -180,84 +178,6 @@ class DefaultListenerAggregate extends Manager implements
             // Change Layout Of Page
             $model = $e->getViewModel();
             $model->setTemplate($layout);
-        }
-    }
-
-    /**
-     * Insert Defined Widgets into Layout Sections(area)
-     *
-     * @param MvcEvent $e MVC Event
-     * @throws \Exception
-     */
-    public function onRenderWidgetator(MvcEvent $e)
-    {
-        $result = $e->getResult();
-        if ($result instanceof Response) {
-            return;
-        }
-
-        $viewModel = $e->getViewModel();
-        if (! $viewModel instanceof ViewModel) {
-            return;
-        }
-
-        // load widgets into {
-        $themeObject  = $this->manager->getThemeObject();
-        if (!$themeObject) {
-            // we are not attained theme
-            return;
-        }
-
-        $sm = $this->sm;
-
-        /*
-         * [
-         *  'layout_name' =>
-         *      [
-         *          'area' => [
-         *              toStringObject,
-         *              ViewModel,
-         *          ]
-         *      ]
-         * ]
-         */
-        $widgetsContainer = array();
-        do {
-            // get merged widgets of child themes
-            $widgetsContainer = ArrayUtils::merge($widgetsContainer, (array) $themeObject->getParam('widgets'));
-        } while($themeObject = $themeObject->getChild());
-
-        $layout           = $viewModel->getTemplate();
-        $areas            = isset($widgetsContainer[$layout]) ? $widgetsContainer[$layout] : array();
-        foreach($areas as $area => $widgets)
-        {
-            if (! is_array($widgets) ) {
-                // convert it to array for itterate over
-                $widgets = array($widgets);
-            }
-
-            foreach ($widgets as $w) {
-                if (is_string($w)) {
-                    if ($sm->has($w))
-                        $w = $sm->get($w);
-                    elseif (class_exists($w))
-                        $w = new $w();
-                }
-
-                if (is_object($w)) {
-                    if ($w instanceof ViewModel)
-                        $viewModel->addChild($w, $area, true);
-                    elseif (method_exists($w, '__toString'))
-                        $w = (string) $w;
-                    else
-                        throw new \Exception('Invalid Widget Provided, Widget "'.get_class($w).'" is not toString implemented or ViewModel instance.');
-                }
-
-                if (is_string($w))
-                    $viewModel->{$area} .= $w;
-                elseif (! $w instanceof ViewModel)
-                    throw new \Exception('Invalid Widget Provided, Widget "'.gettype($w).'"');
-            }
         }
     }
 
