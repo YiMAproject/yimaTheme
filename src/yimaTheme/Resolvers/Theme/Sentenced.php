ae977ddbae3db1ecbc7864f924c3ec8be354986f
Payam Naderi <naderi.payam@gmail.com>
2014-11-24 15:28:34 +0330

    can set default theme by static method

--- src/yimaTheme/Resolvers/Theme/Sentenced.php
+++ src/yimaTheme/Resolvers/Theme/Sentenced.php
@@ -6,10 +6,28 @@ use yimaTheme\Resolvers\ResolverInterface;
 class Sentenced implements
     ResolverInterface
 {
-   protected $name = 'builder';
+    /**
+     * @var string Theme
+     */
+    public $theme;
 
-   public function getName()
-   {
-       return $this->name;
-   }
+    /**
+     * @var string Default Theme
+     */
+    static protected $default_theme = 'default';
+
+    function getName()
+    {
+        return ($this->theme) ? :self::$default_theme;
+    }
+
+    /**
+     * Set Default Template Name
+     *
+     * @param string $name Template Name
+     */
+    static function setDefault($name)
+    {
+        self::$default_theme = (string) $name;
+    }
 }
