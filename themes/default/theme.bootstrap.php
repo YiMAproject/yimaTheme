00b746d43a9e26641badbb9e549af16b999766f0
Payam Naderi <naderi.payam@gmail.com>
2014-11-24 16:24:53 +0330

    add some bootstrap sample

--- themes/default/theme.bootstrap.php
+++ themes/default/theme.bootstrap.php
@@ -1,22 +1,57 @@
 <?php
-use Zend\Mvc\MvcEvent;
-
 /**
  * @var $this \yimaTheme\Theme\Theme
  */
 
-$this->isFinal = false;
-//$this->setCaptureTo('content');
+use Zend\Stdlib\ArrayUtils;
+use Zend\Mvc\MvcEvent;
 
-/** @var $sm \Zend\ServiceManager\ServiceManager */
-$sm = $this->getServiceLocator();
+/**
+ * Theme Resolver Run This Bootstrap And
+ * Fall into Next Theme With Resolver Till Get
+ * Into Final Theme
+ *
+ * By Default is True
+ */
+$this->isFinal = true;
+$this->setTemplate('default-alternate');
+
+// inject variable to view template layout
+$this->setVariable('ip', $_SERVER['REMOTE_ADDR']);
+
+// ---- Register Assets File Into AssetManager Service --------------------------------------------------------------------------------------------\
+/*
+ * These Config must merged to application config at last
+ * : see below
+ */
+$overrideConfigs = array(
+    'asset_manager' => array(
+        'resolver_configs' => array(
+            'paths' => array(
+                __DIR__.DS.'www',
+            ),
+        ),
+    ),
+);
+
+// ---- Merge new config to application merged config ---------------------------------------------------------------------------------------------\
+$mergedConf = $this->getServiceLocator()->get('Config');
+$config     = ArrayUtils::merge($mergedConf, $overrideConfigs);
+
+$this->getServiceLocator()
+    ->setAllowOverride(true)
+    ->setService('config', $config)
+    ->setAllowOverride(false);
+
+
+
 
 // -- Register some autoload ------------------------------------------------
 \Zend\Loader\AutoloaderFactory::factory(array());
 
 // -- Attach an event to event system ---------------------------------------
 /** @var $events \Zend\EventManager\SharedEventManager */
-$events = $sm->get('sharedEventManager');
+$events = $this->getServiceLocator()->get('sharedEventManager');
 $events->attach(
     'Zend\Mvc\Controller\AbstractController',
     MvcEvent::EVENT_DISPATCH,
